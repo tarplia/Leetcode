b30bfa465e80792675e70b496dd9482f190d195c
Sonny Sasaka <sonnysasaka@gmail.com>
2020-02-28 23:10:37 -0800

    Add time grapher for problem 921 and fix code style
    Change-Id: I6ca7e0fb33825df53148376d03fcffbf0cd7f04a

--- 921/E921MinAddValidParantheses.java
+++ 921/E921MinAddValidParantheses.java
@@ -1,15 +1,15 @@
 // Source: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
 
+import java.util.Random;
+
 public class E921MinAddValidParantheses {
     public int minAddToMakeValid(String s) {
         int count = 0;
-        int addParan  = 0;
+        int addParan = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(')
-                count++;
-            else
-                count--;
+            if (s.charAt(i) == '(') count++;
+            else count--;
 
             if (count < 0) {
                 addParan++;
@@ -20,6 +20,22 @@ public class E921MinAddValidParantheses {
         return addParan + Math.abs(count);
     }
 
+    static class TestCaseGeneratorImpl implements TestCaseGenerator {
+        private String generateRandomTestCase(int n) {
+            char[] chars = new char[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++) {
+                chars[i] = r.nextInt(2) == 1 ? '(' : ')';
+            }
+            return new String(chars);
+        }
+
+        @Override
+        public void runTestCase(int n) {
+            new E921MinAddValidParantheses().minAddToMakeValid(generateRandomTestCase(n));
+        }
+    }
+
     public static void main(String[] args) {
         E921MinAddValidParantheses program = new E921MinAddValidParantheses();
 
@@ -27,5 +43,7 @@ public class E921MinAddValidParantheses {
         System.out.println(program.minAddToMakeValid(""));
         System.out.println(program.minAddToMakeValid("()))("));
         System.out.println(program.minAddToMakeValid(")(()"));
+
+        TimeGrapher.graphTime(new TestCaseGeneratorImpl(), 10000000);
     }
 }
