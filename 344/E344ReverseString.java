101c24278f306799533195988a2f044f3503ecde
Lia Pratomo <lia.prtm@gmail.com>
2020-03-22 07:10:42 -0700

    Add time grapher
    Change-Id: I72e68ba994061a12d9d0f55d03dd00d0bdd0762c

--- 344/E344ReverseString.java
+++ 344/E344ReverseString.java
@@ -1,5 +1,7 @@
 // Source: https://leetcode.com/problems/reverse-string/
 
+import java.util.Random;
+
 public class E344ReverseString {
     public void reverseString(char[] s) {
         for (int i = 0; i < s.length/2; i++) {
@@ -19,11 +21,29 @@ public class E344ReverseString {
         System.out.println();
     }
 
+    static class TestCaseGeneratorImpl implements TestCaseGenerator {
+        private char[] generateRandomTestCase(int n) {
+            char[] chars = new char[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++) {
+                chars[i] = (char)(r.nextInt(26) + 65);
+            }
+            return chars;
+        }
+
+        @Override
+        public void runTestCase(int n) {
+            new E344ReverseString().reverseString(generateRandomTestCase(n));
+        }
+    }
+
     public static void main(String[] args) {
         E344ReverseString program = new E344ReverseString();
 
         program.test(new char[] {'h', 'e', 'l', 'l', 'o'});
         program.test(new char[] {'A', 'b', 'C'});
         program.test(new char[] {});
+
+        TimeGrapher.graphTime(new TestCaseGeneratorImpl(), 10000000);
     }
 }
