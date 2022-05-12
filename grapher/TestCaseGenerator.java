e735334a3db1cce188206e736f344e1596ddf6d7
Lia Pratomo <lia.prtm@gmail.com>
2022-05-11 21:27:27 -0700

    Rearrange folders pt 2
    Change-Id: I363a68fd31b88dadba91468aa29565177f11dac4

--- grapher/TestCaseGenerator.java
+++
@@ -1,9 +0,0 @@
-/** Implemented by an algorithm tester that runs a program against generated test cases. */
-public interface TestCaseGenerator {
-    /**
-     * Runs a program against a generated test case.
-     *
-     * @param n the input size of the test case
-     */
-    public void runTestCase(int n);
-}
--- interview-questions-leetcode/Leetcode-Questions-master/README.md
+++
@@ -1,4 +0,0 @@
-# Leetcode-Questions
-Leetcode question list by companies, include the premium questions.
-
-Last updated- 2nd December 2019
